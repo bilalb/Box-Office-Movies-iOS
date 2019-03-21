9c2158bda9c26e966ed0e1d80b2051c4d1987053
Bilal Benlarbi <bilal.benlarbi@neofonie.de>
2019-03-21 18:21:57 +0100

    Initial Commit

---
+++ Box-Office-Movies/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Box-Office-Movies
+//
+//  (c) Neofonie Mobile GmbH (2019)
+//
+//  This computer program is the sole property of Neofonie
+//  Mobile GmbH (http://mobile.neofonie.de) and is protected
+//  under the German Copyright Act (paragraph 69a UrhG).
+//
+//  All rights are reserved. Making copies, duplicating,
+//  modifying, using or distributing this computer program
+//  in any form, without prior written consent of Neofonie
+//  Mobile GmbH, is prohibited.
+//
+//  Violation of copyright is punishable under the German
+//  Copyright Act (paragraph 106 UrhG).
+//
+//  Removing this copyright statement is also a violation.
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Box-Office-Movies/MasterViewController.swift
@@ -0,0 +1,102 @@
+//
+//  MasterViewController.swift
+//  Box-Office-Movies
+//
+//  (c) Neofonie Mobile GmbH (2019)
+//
+//  This computer program is the sole property of Neofonie
+//  Mobile GmbH (http://mobile.neofonie.de) and is protected
+//  under the German Copyright Act (paragraph 69a UrhG).
+//
+//  All rights are reserved. Making copies, duplicating,
+//  modifying, using or distributing this computer program
+//  in any form, without prior written consent of Neofonie
+//  Mobile GmbH, is prohibited.
+//
+//  Violation of copyright is punishable under the German
+//  Copyright Act (paragraph 106 UrhG).
+//
+//  Removing this copyright statement is also a violation.
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [Any]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        navigationItem.rightBarButtonItem = addButton
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+
+    @objc
+    func insertNewObject(_ sender: Any) {
+        objects.insert(NSDate(), at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row] as! NSDate
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let object = objects[indexPath.row] as! NSDate
+        cell.textLabel!.text = object.description
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+
+
+}
+
