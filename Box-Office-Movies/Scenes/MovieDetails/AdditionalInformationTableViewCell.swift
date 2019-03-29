bd412b50407dd49326fed55ef485e1730869f011
Bilal Benlarbi <bilal.benlarbi@neofonie.de>
2019-03-29 19:47:45 +0100

    copyrights fixed

--- Box-Office-Movies/Scenes/MovieDetails/AdditionalInformationTableViewCell.swift
+++ Box-Office-Movies/Scenes/MovieDetails/AdditionalInformationTableViewCell.swift
@@ -2,21 +2,9 @@
 //  AdditionalInformationTableViewCell.swift
 //  Box-Office-Movies
 //
-//  (c) Neofonie Mobile GmbH (2019)
+//  Created by Bilal Benlarbi on 24.03.2019.
+//  Copyrights © 2019 Bilal Benlarbi. All rights reserved.
 //
-//  This computer program is the sole property of Neofonie
-//  Mobile GmbH (http://mobile.neofonie.de) and is protected
-//  under the German Copyright Act (paragraph 69a UrhG).
-//
-//  All rights are reserved. Making copies, duplicating,
-//  modifying, using or distributing this computer program
-//  in any form, without prior written consent of Neofonie
-//  Mobile GmbH, is prohibited.
-//
-//  Violation of copyright is punishable under the German
-//  Copyright Act (paragraph 106 UrhG).
-//
-//  Removing this copyright statement is also a violation.
 
 import UIKit
 
