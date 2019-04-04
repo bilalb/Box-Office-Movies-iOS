9bbd3f4def668cd90964fb6d56fc48d99e155030
Bilal Benlarbi <bilal.benlarbi@neofonie.de>
2019-04-04 20:41:38 +0200

    core-framework Box-Office-Movies-Core framework created

---
+++ Box-Office-Movies-Core/Box-Office-Movies-Core/Box_Office_Movies_Core.h
@@ -0,0 +1,31 @@
+//
+//  Box_Office_Movies_Core.h
+//  Box-Office-Movies-Core
+//
+//  (c) Neofonie Mobile GmbH (2019)
+//
+//  This computer program is the sole property of Neofonie
+//  Mobile GmbH (http://mobile.neofonie.de) and is protected
+//  under the German Copyright Act (paragraph 69a UrhG).
+//
+//  All rights are reserved. Making copies, duplicating,
+//  modifying, using or distributing this computer program
+//  in any form, without prior written consent of Neofonie
+//  Mobile GmbH, is prohibited.
+//
+//  Violation of copyright is punishable under the German
+//  Copyright Act (paragraph 106 UrhG).
+//
+//  Removing this copyright statement is also a violation.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Box_Office_Movies_Core.
+FOUNDATION_EXPORT double Box_Office_Movies_CoreVersionNumber;
+
+//! Project version string for Box_Office_Movies_Core.
+FOUNDATION_EXPORT const unsigned char Box_Office_Movies_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Box_Office_Movies_Core/PublicHeader.h>
+
+
---
+++ Box-Office-Movies-Core/Box-Office-Movies-CoreTests/Box_Office_Movies_CoreTests.swift
@@ -0,0 +1,46 @@
+//
+//  Box_Office_Movies_CoreTests.swift
+//  Box-Office-Movies-Core
+//
+//  (c) Neofonie Mobile GmbH (2019)
+//
+//  This computer program is the sole property of Neofonie
+//  Mobile GmbH (http://mobile.neofonie.de) and is protected
+//  under the German Copyright Act (paragraph 69a UrhG).
+//
+//  All rights are reserved. Making copies, duplicating,
+//  modifying, using or distributing this computer program
+//  in any form, without prior written consent of Neofonie
+//  Mobile GmbH, is prohibited.
+//
+//  Violation of copyright is punishable under the German
+//  Copyright Act (paragraph 106 UrhG).
+//
+//  Removing this copyright statement is also a violation.
+
+import XCTest
+@testable import Box_Office_Movies_Core
+
+class Box_Office_Movies_CoreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
