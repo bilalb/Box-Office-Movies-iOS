74be418def52e0d142ec02730b39c8a4c0af1fa6
Bilal Benlarbi <bilal.benlarbi@neofonie.de>
2019-04-05 13:50:35 +0200

    copyrights fixed

--- Box-Office-Movies-Core/Box-Office-Movies-CoreTests/Box_Office_Movies_CoreTests.swift
+++ Box-Office-Movies-Core/Box-Office-Movies-CoreTests/Box_Office_Movies_CoreTests.swift
@@ -2,21 +2,9 @@
 //  Box_Office_Movies_CoreTests.swift
 //  Box-Office-Movies-Core
 //
-//  (c) Neofonie Mobile GmbH (2019)
+//  Created by Bilal Benlarbi on 04.04.2019.
+//  Copyrights © 2019 Bilal Benlarbi. All rights reserved.
 //
-//  This computer program is the sole property of Neofonie
-//  Mobile GmbH (http://mobile.neofonie.de) and is protected
-//  under the German Copyright Act (paragraph 69a UrhG).
-//
-//  All rights are reserved. Making copies, duplicating,
-//  modifying, using or distributing this computer program
-//  in any form, without prior written consent of Neofonie
-//  Mobile GmbH, is prohibited.
-//
-//  Violation of copyright is punishable under the German
-//  Copyright Act (paragraph 106 UrhG).
-//
-//  Removing this copyright statement is also a violation.
 
 import XCTest
 @testable import Box_Office_Movies_Core
