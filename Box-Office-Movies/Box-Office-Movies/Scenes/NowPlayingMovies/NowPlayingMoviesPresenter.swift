72516feefb093fb21f7b0144ffe99bdf326268a4
Bilal Benlarbi <bilal.benlarbi@neofonie.de>
2019-06-04 18:02:50 +0200

    movie items factored

--- Box-Office-Movies/Box-Office-Movies/Scenes/NowPlayingMovies/NowPlayingMoviesPresenter.swift
+++ Box-Office-Movies/Box-Office-Movies/Scenes/NowPlayingMovies/NowPlayingMoviesPresenter.swift
@@ -6,6 +6,7 @@
 //  Copyrights © 2019 Bilal Benlarbi. All rights reserved.
 //
 
+import Box_Office_Movies_Core
 import UIKit
 
 protocol NowPlayingMoviesPresentationLogic {
@@ -23,25 +24,23 @@ extension NowPlayingMoviesPresenter: NowPlayingMoviesPresentationLogic {
     
     func presentNowPlayingMovies(response: NowPlayingMovies.FetchNowPlayingMovies.Response) {
         DispatchQueue.main.async {
-            let movieItems = response.movies?.compactMap({ movie -> MovieItem in
-                return MovieItem(title: movie.title)
-            })
+            let items = self.movieItems(for: response.movies)
             let shouldHideErrorView = response.error == nil
             let errorDescription = response.error?.localizedDescription
-            let viewModel = NowPlayingMovies.FetchNowPlayingMovies.ViewModel(movieItems: movieItems, shouldHideErrorView: shouldHideErrorView, errorDescription: errorDescription)
+            let viewModel = NowPlayingMovies.FetchNowPlayingMovies.ViewModel(movieItems: items,
+                                                                             shouldHideErrorView: shouldHideErrorView,
+                                                                             errorDescription: errorDescription)
             self.viewController?.displayNowPlayingMovies(viewModel: viewModel)
         }
     }
     
     func presentNextPage(response: NowPlayingMovies.FetchNextPage.Response) {
         DispatchQueue.main.async {
-            let movieItems = response.movies?.compactMap({ movie -> MovieItem in
-                return MovieItem(title: movie.title)
-            })
+            let items = self.movieItems(for: response.movies)
             let shouldPresentErrorAlert = response.error != nil
             let errorAlertMessage = response.error?.localizedDescription
             let errorAlertActions = [UIAlertAction(title: NSLocalizedString("ok", comment: "ok"), style: .cancel)]
-            let viewModel = NowPlayingMovies.FetchNextPage.ViewModel(movieItems: movieItems,
+            let viewModel = NowPlayingMovies.FetchNextPage.ViewModel(movieItems: items,
                                                                      shouldPresentErrorAlert: shouldPresentErrorAlert,
                                                                      errorAlertTitle: nil,
                                                                      errorAlertMessage: errorAlertMessage,
@@ -52,22 +51,18 @@ extension NowPlayingMoviesPresenter: NowPlayingMoviesPresentationLogic {
     }
     
     func presentFilterMovies(response: NowPlayingMovies.FilterMovies.Response) {
-        let movieItems = response.movies?.compactMap({ movie -> MovieItem in
-            return MovieItem(title: movie.title)
-        })
-        let viewModel = NowPlayingMovies.FilterMovies.ViewModel(movieItems: movieItems)
+        let items = movieItems(for: response.movies)
+        let viewModel = NowPlayingMovies.FilterMovies.ViewModel(movieItems: items)
         viewController?.displayFilterMovies(viewModel: viewModel)
     }
     
     func presentRefreshMovies(response: NowPlayingMovies.RefreshMovies.Response) {
         DispatchQueue.main.async {
-            let movieItems = response.movies?.compactMap({ movie -> MovieItem in
-                return MovieItem(title: movie.title)
-            })
+            let items = self.movieItems(for: response.movies)
             let shouldPresentErrorAlert = response.error != nil
             let errorAlertMessage = response.error?.localizedDescription
             let errorAlertActions = [UIAlertAction(title: NSLocalizedString("ok", comment: "ok"), style: .cancel)]
-            let viewModel = NowPlayingMovies.RefreshMovies.ViewModel(movieItems: movieItems,
+            let viewModel = NowPlayingMovies.RefreshMovies.ViewModel(movieItems: items,
                                                                      shouldPresentErrorAlert: shouldPresentErrorAlert,
                                                                      errorAlertTitle: nil,
                                                                      errorAlertMessage: errorAlertMessage,
@@ -75,5 +70,15 @@ extension NowPlayingMoviesPresenter: NowPlayingMoviesPresentationLogic {
                                                                      errorAlertActions: errorAlertActions)
             self.viewController?.displayRefreshMovies(viewModel: viewModel)
         }
+    }
+}
+
+extension NowPlayingMoviesPresenter {
+    
+    func movieItems(for movies: [Movie]?) -> [MovieItem]? {
+        let movieItems = movies?.compactMap({ movie -> MovieItem in
+            return MovieItem(title: movie.title)
+        })
+        return movieItems
     }
 }
