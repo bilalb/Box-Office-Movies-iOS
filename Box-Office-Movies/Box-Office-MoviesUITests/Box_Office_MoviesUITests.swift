9c92f5f5687a370ec762aacb35bb66572e0461b6
Bilal <bilal@benlarbi.fr>
2019-09-15 21:20:42 +0200

    snapshot config improved

--- Box-Office-Movies/Box-Office-MoviesUITests/Box_Office_MoviesUITests.swift
+++ Box-Office-Movies/Box-Office-MoviesUITests/Box_Office_MoviesUITests.swift
@@ -9,7 +9,9 @@
 import XCTest
 
 class Box_Office_MoviesUITests: XCTestCase {
-
+    
+    var locale: Locale = .french
+    
     override func setUp() {
         let application = XCUIApplication()
         setupSnapshot(application)
@@ -17,8 +19,85 @@ class Box_Office_MoviesUITests: XCTestCase {
     }
 
     func testScreenshots() {
+        let application = XCUIApplication()
+        
         snapshot("01MovieList")
-        XCUIApplication().cells.firstMatch.tap()
-        snapshot("02MovieDetails")
+        
+        application.searchFields.firstMatch.tap()
+        locale.searchKeys.forEach { (key) in
+            application.keys[key].tap()
+        }
+        snapshot("02SearchInput")
+        
+        application.cells.firstMatch.tap()
+        snapshot("03MovieDetails")
+        
+        let backButton = application.navigationBars.buttons.element(boundBy: 0)
+        backButton.tap()
+        
+        let keyboardSearchButton = application.buttons[locale.keyboardSearchButtonIdentifier]
+        keyboardSearchButton.tap()
+        
+        let cancelSearchButton: XCUIElement = {
+            let predicate = NSPredicate(format: "label == %@", locale.cancelSearchButtonLabel)
+            return application.buttons.element(matching: predicate)
+        }()
+        cancelSearchButton.tap()
+        
+        let favoriteSegmentIndex = 1
+        application.segmentedControls.firstMatch.tap(at: favoriteSegmentIndex)
+        snapshot("04Favorites")
+    }
+}
+
+extension Box_Office_MoviesUITests {
+    
+    enum Locale: String {
+        
+        case english
+        case french
+        
+        var searchKeys: [String] {
+            switch self {
+            case .english:
+                return ["T", "h"]
+            case .french:
+                return ["L", "a"]
+            }
+        }
+        
+        var keyboardSearchButtonIdentifier: String {
+            switch self {
+            case .english:
+                return "Search"
+            case .french:
+                return "Rechercher"
+            }
+        }
+        
+        var cancelSearchButtonLabel: String {
+            switch self {
+            case .english:
+                return "Cancel"
+            case .french:
+                return "Annuler"
+            }
+        }
+    }
+}
+
+extension XCUIElement {
+    
+    /// Helper method to tap a UISegmentedControl's segment.
+    ///
+    /// - Parameter index: The index of segment to tap.
+    func tap(at index: Int) {
+        guard buttons.count > 0 else {
+            return
+        }
+        let segmentsIndexes = 0 ..< buttons.count
+        var segments = segmentsIndexes.map { buttons.element(boundBy: $0) }
+        segments.sort { $0.frame.origin.x < $1.frame.origin.x }
+        segments[index].tap()
     }
 }
