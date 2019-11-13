fd2791081e3903a32f4253c7f0bd6ed08f76ce18
Bilal Benlarbi <>
2019-11-13 14:24:02 +0100

    CloudKit support added

--- Box-Office-Movies-Core/Box-Office-Movies-Core/Persistence/CoreDataStack.swift
+++ Box-Office-Movies-Core/Box-Office-Movies-Core/Persistence/CoreDataStack.swift
@@ -14,7 +14,13 @@ class CoreDataStack {
     static var shared = CoreDataStack()
     
     lazy var persistentContainer: NSPersistentContainer = {
-        let persistentContainer = NSPersistentContainer(name: Constants.CoreData.dataModelName, bundle: Bundle(for: ManagerProvider.self))
+        let persistentContainer: NSPersistentContainer = {
+            if #available(iOS 13.0, *) {
+                return NSPersistentCloudKitContainer(name: Constants.CoreData.dataModelName, bundle: Bundle(for: ManagerProvider.self))
+            } else {
+                return NSPersistentContainer(name: Constants.CoreData.dataModelName, bundle: Bundle(for: ManagerProvider.self))
+            }
+        }()
 
         persistentContainer.loadPersistentStores { (_, error) in
             guard let error = error as NSError? else { return }
